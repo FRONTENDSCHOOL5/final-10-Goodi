b5aef23a3149e33bc19db77074c2692898cb795a
taehee.rho <taeheerho@gmail.com>
2023-06-06 03:13:17 +0900

    Docs: layout 문서 구조 수정 및 생성(#4)

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Navigation() {
+  return (
+    <NavigationLayout>
+      <li>this is Navigation</li>
+    </NavigationLayout>
+  );
+}
+
+const NavigationLayout = styled.ul``;
---
+++ src/layout/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Navigation from "../components/Navigation";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <Navigation />
+      <Wrapper>{children}</Wrapper>
+      <Footer />
+    </>
+  );
+}
+
+const Wrapper = styled.div``;
