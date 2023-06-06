eaabcc9324c1ea992aeb2d22ff99918de6cdcc47
taehee.rho <taeheerho@gmail.com>
2023-06-07 00:24:40 +0900

    Docs: Detail 페이지 생성및 App.js 연결(#10)

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import GlobalStyle from "./style/GlobalStyle";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Main from './pages/Main';
 import Login from './pages/Login';
+import Detail from './pages/Detail';
 
 function App() {
   return (
     <BrowserRouter>
       <Link to="/login"></Link>
       <GlobalStyle />
-        <Routes>
-          <Route path="/main" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+      <Routes>
+        <Route path="/main" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
     </BrowserRouter>
   );
 }
