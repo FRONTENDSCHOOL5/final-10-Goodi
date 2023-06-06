64bb15cc9030d17fc2bcba9c1fc47619c923d2c3
nkEllaaa <dhrtns515@gmail.com>
2023-06-06 13:04:37 +0900

    Design: loginDef page 폰트, sns이미지 외 완료(#3)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,9 +8,9 @@ export default function Login() {
   return (
     <LayoutDiv>
       <H1>로그인 페이지</H1>
-      <H2>Welcome to
-      <img src={SymbolImage} alt="Symbol" />
-      </H2>
+      <div>
+      <H2>Welcome to<img src={SymbolImage} alt="Symbol"/></H2>
+      </div>
       <InputDiv>
         <Label>이메일</Label>
         <InputBox 
@@ -18,7 +18,6 @@ export default function Login() {
           height="48px" 
           padding="15px"
           onChange={() => {}}
-          className="inputBox"
           placeholder="이메일을 입력해주세요"
         />
         </InputDiv>
@@ -27,35 +26,68 @@ export default function Login() {
         <InputBox 
           width="432px" 
           height="48px" 
-          padding="15px"
           onChange={() => {}}
           type="password"
-          className="inputBox"
           placeholder="비밀번호를 입력하세요"
         />
       </InputDiv>
-      <div style={{margin: "55px"}}>
+      <ButtonDiv>
         <ButtonDef 
           type="button" 
+          bg="black"
           width="432px" 
           height="56px" 
           br="4px">로그인</ButtonDef>
+        <span>SNS 로그인</span>  
+      </ButtonDiv>
+      <SnsDiv>
+        <SnsBg bg="#FAE64D">
+          <img src="" alt="" />
+        </SnsBg>
+        <SnsBg bg="var(--gray100-color)">
+          <img src="" alt="" />
+        </SnsBg>
+        <SnsBg bg="#5693FF">
+          <img src="" alt="" />
+        </SnsBg>
+      </SnsDiv>
+      <div>
+      <p>아직 구디 회원이 아니세요?</p>
+      <button type="button" className="join_button">회원가입 하기</button>
       </div>
     </LayoutDiv>
   );
 }
 
 export const LayoutDiv = styled.div`
+  width: 432px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 81px;
+  margin:0 auto;
+  padding: 81px 0;
+
+  p {
+    font-size: 1rem;
+    color: var(--gray500-color);
+    display: inline;
+    margin-right: 17px;
+  }
+  button {
+    font-weight: 600;
+    display: inline;
+    border-bottom: 2px solid black;
+    padding: 6px 0px;
+  }
+  .join_button {
+    font-size: 1.25rem;
+  }
 `;
 
 export const InputDiv = styled.div`
   display: flex;
   flex-direction: column;
-  margin-bottom:24px;
+  margin-top:24px;
 `;
 
 export const H1 = styled.h1`
@@ -68,14 +100,42 @@ export const H1 = styled.h1`
   padding: 0;
   position: absolute;
 `
-
-export const H2 = styled.h2`
+export const H2 = styled.div`
   font-size: 2.5rem;
   font-weight: 700;
-  margin-bottom: 40px;
-`
+  margin-bottom: 16px;
+  display: inline;
 
+  img {
+    vertical-align: text-bottom;
+  }
+` 
 export const Label = styled.label`
   margin-bottom: 8px;
   font-weight: 700;
+`
+export const ButtonDiv = styled.div`
+  padding: 55px 0;
+  border-bottom: 1px solid var(--gray200-color);
+  position: relative;
+  span {
+    background-color: white;
+    padding: 10px;
+    color: var(--gray200-color);
+    position:absolute;
+    top: 91%;
+    left: 50%;
+    transform: translate(-50%);
+  }
+`
+export const SnsDiv = styled.div`
+  display: flex;
+  gap: 24px;
+  padding: 44px 0px 81px;
+`
+export const SnsBg = styled.div`
+  background-color: ${ props => props.bg };
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
 `
