870098e0e5e9be401435857d8ec78f7bee2033cc
taehee.rho <taeheerho@gmail.com>
2023-06-07 16:19:27 +0900

    chore: product Mock 데이터 생성 및 Detail 페이지 에 연결

---
+++ src/mock/productData.js
@@ -0,0 +1,15 @@
+
+const ProductData = [
+    {
+        "id": 1,
+        "user_profile": "https://image1.marpple.co/files/u_1470231/2023/2/original/8ac0bf16c3a6eef8b0f1b5f4a9bb6b0304f5f4531.png?q=92&w=600&f=jpeg&bg=f6f6f6",
+        "user_name": "무쿠무쿠 스튜디오",
+        "user_email": "dksljdf@gmail.com",
+        "propduct_img": ["https://image1.marpple.co/files/u_1470231/2023/5/original/07fd94acc9b63dec50682e3a8220c6171c44707b1.png?q=92&w=432&f=jpeg&bg=f6f6f6", "https://image1.marpple.co/files/u_1470231/2023/5/original/c5259af124c93e5d763739f130a4ddd03747c1061.png?q=92&w=432&f=jpeg&bg=f6f6f6", "https://image1.marpple.co/files/u_1470231/2023/5/original/e62c081ab5f1d632d56958ac82b91b737887bf701.png?q=92&w=432&f=jpeg&bg=f6f6f6", "https://image1.marpple.co/files/u_1470231/2023/5/original/173cb3d6e5bfba450e0502905a6a5c61e7ec48251.png?q=92&w=432&f=jpeg&bg=f6f6f6"],
+        "product_title": "무쿠무쿠 투명 핸드폰케이스",
+        "product_description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,",
+        "price": "20000"
+    },
+];
+
+export default ProductData;
