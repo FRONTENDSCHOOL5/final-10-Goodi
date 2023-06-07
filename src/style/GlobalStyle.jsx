9bca3352cc9f298051f5bf9a84117c620d6fe4d3
Oh Ga Eun <dhrkdms4113@naver.com>
2023-06-07 11:37:14 +0900

    Style: a11y-hidden 화면 숨김처리 코드 추가

--- src/style/GlobalStyle.jsx
+++ src/style/GlobalStyle.jsx
@@ -33,6 +33,17 @@ const GlobalStyle = createGlobalStyle`
   img {
     vertical-align: top;
   }
+
+  .a11y-hidden {
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+  }
 `;
 
 export default GlobalStyle;
