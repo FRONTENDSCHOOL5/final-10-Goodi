fd4921605f1480220a00498e87f0a753e0f13e57
Oh Ga Eun <dhrkdms4113@naver.com>
2023-06-13 15:03:33 +0900

    feat: 로그인 API 연동

---
+++ src/api/login.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const LOGIN_URL = "https://api.mandarin.weniv.co.kr/user/login";
+
+
+const instance = axios.create({
+  baseURL: LOGIN_URL,
+  headers: {
+    "Content-Type": "application/json"
+  }
+
+})
+
+
+
+// test 계정 정보 : suritest@test.com / suritest
+
+const fetchData = async (loginData) => {
+  try {
+    const res = await instance.post(LOGIN_URL, loginData);
+    console.log('결과', res);
+
+
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+console.log(fetchData());
+
+
+export default fetchData;
