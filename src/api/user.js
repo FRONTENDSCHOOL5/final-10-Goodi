ead36ac09431bc1d1742705065b9714630ed01e4
Oh Ga Eun <dhrkdms4113@naver.com>
2023-06-14 09:15:43 +0900

    Feat: 회원가입 API 연동

--- src/api/user.js
+++ src/api/user.js
@@ -1,1 +1,28 @@
-// api 연결 예정
+import { goodiFetchInstance } from './instance';
+
+export const postUserPath = '/user';
+export const postUser = async ({
+  username,
+  email,
+  password,
+  accountname,
+  intro,
+  image,
+}) => {
+  // 요 생성된 기본 설정을 들고있는 Axios 인스턴스를 통해서, 회원가입 API 를 호출해주는 js 함수를 만들었어요.
+  // 첫번째 인자에는 엔드포인트 뒤의 path 가 들어가고요,
+  // 두번째 인자에는 Request Body 가 들어가요.
+  // data 에는 Response 가 담겨있어요.
+  const data = await goodiFetchInstance.post(postUserPath, {
+    user: {
+      username,
+      email,
+      password,
+      accountname,
+      intro,
+      image
+    }
+  })
+
+  return data;
+};
---
+++ src/hook/useJoin.js
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react';
+import { postUser } from '../api/user';
+
+// 우리는 API 의 결과값을 상태로 잘 관리할 수 있어야해요. 따라서 요 친구를 Hooks 로 만들었어요.
+export const useJoin = () => {
+  // 이 상태는 회원가입이 성공했을 때의 상태
+  const [joinResult, setJoinResult] = useState(null);
+  // 이 상태는 회원가입이 실패했을 때의 상태
+  const [errorResult, setErrorResult] = useState(null);
+  // 우리는 이 상태를 통해, API 를 호출할 때 사용할 Request Body 를 가지고 올 수 있어요.
+  const [mutateInfo, callMutate] = useState(null);
+  const [bounce, setBounce] = useState(false);
+
+  // 이 함수를 통해 API 를 호출하는 부분을 내부 메소드로 정의를 했어요.
+  const callJoin = async (userInfo) => {
+    const { data } = await postUser(userInfo)
+
+    return data
+  }
+
+  useEffect(() => {
+    // API 를 호출하는 함수에요. (useEffect 는 Async 함수를 다룰때 아래와 같이 다뤄요)
+    const fetchData = async () => {
+      try {
+        // 성공한 케이스
+        const data = await callJoin(mutateInfo)
+        setJoinResult(data)
+      } catch (e) {
+        // 서버에서 에러를 내려준 케이스
+        const data = e.response.data
+        setErrorResult(data)
+      } finally {
+        setBounce(false)
+      }
+    }
+
+    // 만약 최초 시도이면서, callMutate 를 통해 데이터가 들어왔으면,
+    if (bounce && mutateInfo != null) {
+      // API 를 호출해요.
+      fetchData()
+    }
+  }, [mutateInfo, setJoinResult])
+
+
+  useEffect(() => {
+    if (!bounce) {
+      setBounce(true)
+    }
+  }, [bounce, callMutate])
+
+  return { joinResult, errorResult, callMutate }
+};
