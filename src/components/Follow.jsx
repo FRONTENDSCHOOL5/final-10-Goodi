7421d59ee50db9704d97805b0235d6dcce0a08b7
Oh Ga Eun <dhrkdms4113@naver.com>
2023-06-16 00:40:51 +0900

    Design: profile 페이지 레이아웃 변경-좌측(#102)

---
+++ src/components/Follow.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import followers from "../assets/Followers.svg"
+import styled from 'styled-components'
+import ProductData from '../mock/productData'
+import ProfileUI from './common/ProfileUI';
+
+export default function Follow() {
+  const followList = ProductData;
+
+  return (
+    <FollowWrap>
+      <h2><img src={followers} alt="" /></h2>
+      {followList.map((item) => (
+        <ProfileUI
+          user_profile={item.profile}
+          user_name={item.name}
+          user_email={item.email}
+        />
+      )).slice(0, 3)}
+    </FollowWrap>
+  )
+}
+
+const FollowWrap = styled.article`
+  width: 100%;
+
+  h2 {
+    margin-bottom: 30px;
+  }
+
+  a {
+    img {
+    width: 48px;
+    }
+    p {
+      font-size: 12px;
+    }
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+`
