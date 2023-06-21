a496e8ed685cd532c90aa509fe8a03ce05f8847d
Oh Ga Eun <dhrkdms4113@naver.com>
2023-06-21 09:35:49 +0900

    Docs: Skeleton UI page 폴더 생성

--- src/style/skeletonUI/skeletonPage/UserSkeleton.jsx
+++ src/style/skeletonUI/skeletonPage/UserSkeleton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import SkeletonItem from './SkeletonItem';
+import SkeletonItem from '../SkeletonItem';
 
 const UserSkeleton = () => {
   return (
